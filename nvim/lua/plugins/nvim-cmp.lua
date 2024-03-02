536d0ae05890f7815183dafacc7719987b7f5dd5
Renell Castro <renellc9819@gmail.com>
2024-03-02 01:47:30 -0800

    Use codeium.nvim instead of codeium.vim

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -17,8 +17,8 @@ return {
 					end,
 				},
 				sources = cmp.config.sources({
+					{ name = "codeium", max_item_count = 5 },
 					{ name = "nvim_lsp" },
-					{ name = "codeium" },
 					{ name = "luasnip" },
 					{ name = "buffer" },
 					{ name = "path" },
