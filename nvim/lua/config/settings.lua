ffba00fce7ad1d4d01fe3de8ba73ebdb0627bcc6
Renell Castro <renellc@Renells-MBP.attlocal.net>
2024-08-21 12:27:16 -0700

    Updated tab settings

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -3,6 +3,11 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
 
+-- Tab/Space
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
 -- ufo
 vim.o.foldcolumn = "0"
 vim.o.foldlevel = 99
