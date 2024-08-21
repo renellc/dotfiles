50c0dd0a110950c449c9c3a7dc9daebcc9676703
Renell Castro <renellc@Renells-MBP.attlocal.net>
2024-08-21 11:36:00 -0700

    Added plugins for diagnostics and formatting

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,46 @@
+return {
+	{
+		"williamboman/mason.nvim",
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			require("mason").setup({})
+
+			local mason_lspconfig = require("mason-lspconfig")
+			mason_lspconfig.setup({
+				automatic_installation = true,
+				ensure_installed = {
+					"lua_ls",
+				},
+			})
+
+			mason_lspconfig.setup_handlers({
+				function(server_name)
+					require("lspconfig")[server_name].setup({})
+				end,
+				["lua_ls"] = function()
+					require("lspconfig").lua_ls.setup({
+						settings = {
+							Lua = {
+								diagnostics = {
+									globals = {
+										"vim",
+										"require",
+									},
+								},
+								workspace = {
+									library = vim.api.nvim_get_runtime_file("", true),
+								},
+							},
+						},
+					})
+				end,
+			})
+		end,
+	},
+}
