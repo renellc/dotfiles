48ce31f1253fcb498509cdfec1905cbe26c8dcdc
Renell Castro <renellc@Renells-MBP.attlocal.net>
2024-08-21 15:24:32 -0700

    Added `ensure_installed` languages for treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,5 +2,21 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				auto_install = true,
+				sync_install = false,
+				highlight = {
+					enable = true,
+				},
+				indent = {
+					enable = true,
+				},
+				ensure_installed = {
+					"lua",
+				},
+				ignore_install = {},
+			})
+		end,
 	},
 }
