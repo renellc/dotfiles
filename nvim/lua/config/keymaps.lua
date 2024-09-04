90847876f966d840eee2692d99c6d63ea9d9d24b
Renell Castro <renellc@Renells-MacBook-Pro.local>
2024-09-03 22:11:52 -0700

    Updated keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,7 @@
 vim.keymap.set("n", "<leader>ca", function()
 	vim.lsp.buf.code_action()
 end, {})
+vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, {})
 
 -- Telescope
 local extensions = require("telescope").extensions
@@ -13,8 +14,13 @@ vim.keymap.set("n", "<leader>ff", function()
 	})
 end, {})
 vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fb", function()
+	builtin.buffers({
+		sort_mru = true,
+	})
+end, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>fm", builtin.marks, {})
 
 vim.keymap.set("n", "<leader>fE", function()
 	extensions.file_browser.file_browser({
