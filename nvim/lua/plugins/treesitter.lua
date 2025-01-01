9b11c71d745366d36bca305b6dcf7ce849d1e0e4
Renell Castro <renellc9819@gmail.com>
2025-01-01 02:31:07 -0800

    add treesitter

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,40 @@
+return {
+	{
+		'nvim-treesitter/nvim-treesitter',
+		build = ':TSUpdate',
+		config = function()
+			local configs = require('nvim-treesitter.configs')
+			configs.setup({
+				ensure_installed = {
+					"css",
+					"csv",
+					"dockerfile",
+					"git_rebase",
+					"gitcommit",
+					"gitignore",
+					"hcl",
+					"html",
+					"javascript",
+					"jq",
+					"jsdoc",
+					"json",
+					"lua",
+					"luadoc",
+					"markdown",
+					"markdown_inline",
+					"nginx",
+					"python",
+					"scss",
+					"sql",
+					"tsv",
+					"typescript",
+					"vim",
+					"vimdoc",
+					"yaml",
+				},
+				highlight = { enable = true },
+				indent = { enable = true }
+			})
+		end,
+	}
+}
