628716f93313ff74d5b960ec970c4bbeb239aac0
Renell Castro <renellc9819@gmail.com>
2025-01-04 12:44:47 -0800

    added mini.pairs

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -5,13 +5,20 @@ return {
 			'JoosepAlviste/nvim-ts-context-commentstring'
 		},
 		version = '*',
+		event = 'BufReadPre',
 		opts = {
 			options = {
 				custom_commentstring = function()
 					return require('ts_context_commentstring').calculate_commentstring() or
-					vim.bo.commentstring
+					    vim.bo.commentstring
 				end,
 			}
 		}
-	}
+	},
+	{
+		'echasnovski/mini.pairs',
+		version = '*',
+		event = 'InsertEnter',
+		config = true,
+	},
 }
