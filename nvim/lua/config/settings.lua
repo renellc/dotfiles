08e510d35dbce7d0a7db66bafea2c77d03653c8d
Renell Castro <renellc@Renells-MacBook-Pro.local>
2025-01-27 09:15:56 -0800

    add ufo folding

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -10,3 +10,9 @@ vim.opt.signcolumn = "yes"
 
 -- Colorscheme
 vim.cmd.colorscheme("catppuccin")
+
+-- Folds
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
