bc4c4529c7f771de73e24ded04877159cf72f12a
Renell Castro <renellc@Renells-MBP.attlocal.net>
2025-09-01 16:45:40 -0700

    add lspconfig

---
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,39 @@
+return {
+				"neovim/nvim-lspconfig",
+				config = function()
+								local servers = {
+												cssls = {},
+												docker_compose_language_service = {},
+												emmet_language_server = {},
+												html = {},
+												jsonls = {},
+												lua_ls = {
+																settings = {
+																				Lua = {
+																								runtime = {
+																												version = "LuaJIT"
+																								},
+																								diagnostics = {
+																												globals = { "vim", "require" },
+																								},
+																								workspace = {
+																												library = vim.api.nvim_get_runtime_file("", true)
+																								},
+																								telemetry = {
+																												enable = false,
+																								},
+																				},
+																},
+												},
+												tailwindcss = {},
+												ts_ls = {},
+								}
+
+								local lspconfig = require("lspconfig")
+
+								for server_name, config in pairs(servers) do
+												lspconfig[server_name].setup(config)
+												vim.lsp.enable(server_name)
+								end
+				end,
+}
