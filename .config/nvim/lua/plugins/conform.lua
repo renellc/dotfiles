3b34fd59a04d2dad653532bce5c9d636de360b09
Renell Castro <renellc@Renells-MBP.attlocal.net>
2025-09-01 22:57:05 -0700

    always format on save

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,6 +3,10 @@ return {
 	event = { "BufWritePre" },
 	cmd = { "ConformInfo" },
 	opts = {
+		format_on_save = {
+			timeout_ms = 1000,
+			lsp_format = "fallback",
+		},
 		formatters_by_ft = {
 			lua = { "stylua" },
 			javascript = { "biome", "biome-organize-imports", "prettierd", "prettier", stop_after_first = true },
