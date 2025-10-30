3ab1e75d15993b1d32b27540870a2bea6b0cabd0
Renell Castro <renellc@Renells-MacBook-Pro.local>
2025-10-30 00:39:38 -0700

    update keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Code Actions
-vim.keymap.set("n", "<leader><F2>", vim.lsp.buf.rename, { desc = "Rename" })
-vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Rename" })
-vim.keymap.set("n", "<Shift-K>", vim.lsp.buf.hover, { desc = "Rename" })
-vim.keymap.set("n", "<A-.>", vim.lsp.buf.code_action, { desc = "Rename" })
+vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Rename" })
+vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Goto definition" })
+vim.keymap.set("n", "<Shift-K>", vim.lsp.buf.hover, { desc = "Hover" })
+vim.keymap.set("n", "<A-.>", vim.lsp.buf.code_action, { desc = "Code action(s)" })
