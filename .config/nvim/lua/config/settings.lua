14b6db7a58b802a093324867ce7c3240fd7e8f37
Renell Castro <renellc@Renells-MacBook-Pro.local>
2026-07-12 03:59:37 -0700

    add general settings

---
+++ .config/nvim/lua/config/settings.lua
@@ -0,0 +1,30 @@
+-- Colorscheme settings
+vim.opt.termguicolors = true
+vim.cmd.colorscheme("habamax")
+
+-- Line settings
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 999
+
+-- Tab settings
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+-- Search settings
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- Other settings
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "120"
+vim.opt.showmatch = true
+
