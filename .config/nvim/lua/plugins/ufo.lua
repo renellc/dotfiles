ad8df3da19a3845c9c4f97e2cfad7494775ac66b
Renell Castro <renellc@Renells-MacBook-Pro.local>
2026-07-16 00:19:37 -0700

    add ufo

---
+++ .config/nvim/lua/plugins/ufo.lua
@@ -0,0 +1,15 @@
+return {
+	"kevinhwang91/nvim-ufo",
+	dependencies = { "kevinhwang91/promise-async" },
+	init = function()
+		vim.o.foldcolumn = "1"
+		vim.o.foldlevel = 99
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
+	end,
+	opts = {
+		provider_selector = function(bufnr, filetype, buftype)
+			return { "treesitter", "indent" }
+		end,
+	},
+}
