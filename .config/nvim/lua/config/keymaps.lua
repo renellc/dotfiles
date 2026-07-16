d7e614d46739440752378bddb541d794ee28cb67
Renell Castro <renellc@Renells-MacBook-Pro.local>
2026-07-16 00:52:17 -0700

    update keymaps + point treesitter to correct branch

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -10,3 +10,10 @@ local oil = require("oil")
 vim.keymap.set("n", "<leader>e", function()
 	oil.toggle_float(oil.get_current_dir())
 end, { desc = "oil toggle window" })
+
+-- Editor keymaps
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action(s)" })
+vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Rename" })
+vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Goto definition" })
+vim.keymap.set("n", "<Shift-K>", vim.lsp.buf.hover, { desc = "Hover" })
